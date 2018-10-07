9e97030fe52aae34da6c0fcd6cbb3a51bfb241b4
Pahomov Dmitry <topt.iiiii@gmail.com>
2018-10-07 19:17:23 +0300

    add docs

--- app/com/example/mts/util/ErrorHandler.scala
+++ app/com/example/mts/util/ErrorHandler.scala
@@ -20,37 +20,42 @@ final class ErrorHandler (
   router: => Router
 ) extends DefaultHttpErrorHandler(env, config, sourceMapper, Some(router)) with Logging {
 
+  /** @inheritdoc */
   override def onBadRequest(request: RequestHeader, message: String): Future[Result] = {
     Future.successful {
       Results.BadRequest(Json.obj("error" -> "BadRequest"))
     }
   }
 
+  /** @inheritdoc */
   override def onForbidden(request: RequestHeader, message: String): Future[Result] = {
     Future.successful {
       Results.Forbidden(Json.obj("error" -> "Forbidden"))
     }
   }
 
+  /** @inheritdoc */
   override def onNotFound(request: RequestHeader, message: String): Future[Result] = {
     Future.successful {
       Results.NotFound(Json.obj("error" -> "NotFound"))
     }
   }
 
+  /** @inheritdoc */
   override def onOtherClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
     Future.successful {
       Results.Status(statusCode)(Json.obj("error" -> message))
     }
   }
 
+  /** @inheritdoc */
   override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
     log(exception)
     Future.successful {
       Results.InternalServerError(Json.obj("error" -> exception.getLocalizedMessage))
     }
   }
-
+  
   private def log(exception: Throwable): Unit = {
     logger.error("core onServerError handled exception", exception)
   }
