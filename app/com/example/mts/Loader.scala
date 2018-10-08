f4c9108ee2b560c4c96d382ac65c413b1963ae81
Pahomov Dmitry <topt.iiiii@gmail.com>
2018-10-08 14:22:48 +0300

    add application loader

--- app/com/example/mts/Loader.scala
+++ app/com/example/mts/Loader.scala
@@ -1,5 +1,16 @@
 package com.example.mts
 
-class Loader {
+import com.example.mts.module.MoneyTransferSandboxComponents
+import play.api.{Application, ApplicationLoader, LoggerConfigurator}
+import play.api.ApplicationLoader.Context
 
+final class Loader extends ApplicationLoader {
+  def load(context: Context): Application = {
+    LoggerConfigurator(context.environment.classLoader).foreach {
+      _.configure(context.environment, context.initialConfiguration, Map.empty)
+    }
+    val components = new MoneyTransferSandboxComponents(context)
+
+    components.application
+  }
 }
